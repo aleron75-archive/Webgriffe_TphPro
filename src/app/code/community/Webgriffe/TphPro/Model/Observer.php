97b96fc04753d7d268163c2772276e4c25ed165a
Alessandro Ronchi <aleron75@gmail.com>
2012-11-11 11:33:18 +0100

    First implementation of Observer

--- src/app/code/community/Webgriffe/TphPro/Model/Observer.php
+++ src/app/code/community/Webgriffe/TphPro/Model/Observer.php
@@ -5,10 +5,20 @@
  *
  * @author Alessandro Ronchi <aronchi at webgriffe.com>
  */
-class Observer {
+class Webgriffe_TphPro_Model_Observer {
 
     public function addTemplateHints($observer) {
-        
+        $event = $observer->getEvent();
+        $block = $event->getBlock();
+        $transport = $event->getTransport();
+        $html = '<magento '
+                . ($block->getNameInLayout() == 'root' ? 'handles="' . implode(",", $block->getLayout()->getUpdate()->getHandles()) . '"' : '')
+                . '" block="' . get_class($block)
+                . '" template="' . $block->getTemplate()
+                . '" name="' . $block->getNameInLayout()
+                . '" alias="' . $block->getBlockAlias()
+                . '">' . $transport->getHtml() . '</magento>';
+        $transport->setHtml($html);
     }
 
 }
