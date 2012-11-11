252bfe8321a55d3bbabae5415d6df3a0925837d1
Alessandro Ronchi <aleron75@gmail.com>
2012-11-11 11:12:51 +0100

    added observer empty class

---
+++ src/app/code/community/Webgriffe/TphPro/Model/Observer.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * TODO Description of Observer
+ *
+ * @author Alessandro Ronchi <aronchi at webgriffe.com>
+ */
+class Observer {
+
+    public function addTemplateHints($observer) {
+        
+    }
+
+}
